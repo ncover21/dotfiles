7c81ae821607fa7b5c9de52e1586f2b90372d2c4
Noah Cover <ncover@ucsc.edu>
2020-06-07 21:26:35 -0700

    cleaning file names

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+link(){
+	for file in $( ls -A | grep -vE '\.exclude*|\.git$|\.gitignore|.*.md|.DS_Store|bootstrap.sh' ) ; do
+		ln -sv "$PWD/$file" "$HOME"
+	done
+	echo "$PROMPT Symlinking complete"
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	link;
+else
+	read -p "This will link config files to home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+	    link;
+    fi;
+fi;
